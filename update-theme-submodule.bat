ad946986c72ce7ed64cb706b38a137adc85369fe
Heracles <email@heracl.es>
2020-09-13 21:03:59 +0300

    Update update-theme-submodule.bat

--- update-theme-submodule.bat
+++ update-theme-submodule.bat
@@ -1,5 +1,8 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 rem git submodule update --rebase --remote
+=======
+>>>>>>> b362e2c0b3a7f4b81c79f076f8e7e1e5907fa307
 =======
 >>>>>>> b362e2c0b3a7f4b81c79f076f8e7e1e5907fa307
 cd themes/xerographic
